0fa4e3ed1eb29911eab21ce9480b2863f9de326d
Oscar De Leon Gomez <oleongomez@gmail.com>
2021-11-01 18:31:34 -0700

    Transfer function implemented

--- src/transfer.js
+++ src/transfer.js
@@ -8,10 +8,11 @@ const Transfer = () => {
   const [show_overdraft, setShowOverdraft] = useState(false);
   const [amount, setAmount] = useState(0.0);
   const [updated, setUpdated] = useState(false);
-  const [recipient, setRecipient] = useState({ email: "", unique_id: "" });
+  const [recipientEmail, setRecipientEmail] = useState("");
+  const [recipient, setRecipient] = useState({});
+  const [found, setFound] = useState(false);
 
   const url = "http://localhost:3001/accounts/data";
-  const search_by_email_url = "http://localhost:3001/accounts/data_by_email";
   const update_url = "http://localhost:3001/accounts/update_balance";
 
   const [account, setAccount] = useState({});
@@ -40,29 +41,49 @@ const Transfer = () => {
     console.log("Start Transfer");
     console.log("amount: ", amount);
     console.log("amount as float: ", parseFloat(amount));
-    axios
+    let result = axios
       .post(update_url, { ...account, amount: parseFloat(amount) * -1 })
       .then((value) => {
         console.log("handle_withdraw: ", value);
         if (value.data === "INVALID_OPERATION_OVERDRAFT") {
           console.log("Overdraft protection");
           setShowOverdraft(true);
-        } else {
-          setAccount(value);
-          setUpdated(true);
-          setAmount(0.0);
-          setShowOverdraft(false);
+          return;
         }
+        if (value.data === "NON_EXISTENT_ACCOUNT") {
+          console.log("Account does not exists", value.data);
+          return;
+        }
+        console.log("I have deducted the amount");
+        console.log("Will search for recipient:", recipientEmail);
+        setAccount(value);
+        setShowOverdraft(false);
+        return axios
+          .post(update_url, {
+            ...recipient,
+            amount: parseFloat(amount),
+          })
+          .then((response) => {
+            console.log(response);
+          });
       });
+    setUpdated(true);
+    setAmount(0.0);
+    console.log("Result: ", result);
   };
 
   const handle_search = () => {
     console.log("Start Search");
-    axios.post(search_by_email_url, { email: recipient }).then((res) => {
-      console.log("recipient search data", res.data);
-    }).catch(err=>{
-        console.log(err)
-    });
+    axios
+      .post(url, { email: recipientEmail })
+      .then((res) => {
+        console.log("recipient search data", res.data);
+        setRecipient(res.data);
+        setFound(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -85,7 +106,7 @@ const Transfer = () => {
                       type="email"
                       placeholder="name@example.com"
                       onChange={(e) => {
-                        setRecipient(e.currentTarget.value);
+                        setRecipientEmail(e.currentTarget.value);
                       }}
                     />
                   </FloatingLabel>
@@ -98,31 +119,35 @@ const Transfer = () => {
                   </Button>
                 </Form.Group>
               </Row>
-              <Row className="mb-3">
-                <Form.Group>
-                  <FloatingLabel
-                    label="Amount to transfer"
-                    className="form-label mb-3"
-                  >
-                    <Form.Control
-                      type="input"
-                      id="amount"
-                      placeholder="Amount to withdraw"
-                      value={amount}
-                      onChange={(e) => {
-                        setAmount(e.currentTarget.value);
-                      }}
-                    />
-                  </FloatingLabel>
-                  <Button
-                    type="submit"
-                    className="btn-primary"
-                    onClick={handle_transfer}
-                  >
-                    Transfer
-                  </Button>
-                </Form.Group>
-              </Row>
+              {found ? (
+                <Row className="mb-3">
+                  <Form.Group>
+                    <FloatingLabel
+                      label="Amount to transfer"
+                      className="form-label mb-3"
+                    >
+                      <Form.Control
+                        type="input"
+                        id="amount"
+                        placeholder="Amount to withdraw"
+                        value={amount}
+                        onChange={(e) => {
+                          setAmount(e.currentTarget.value);
+                        }}
+                      />
+                    </FloatingLabel>
+                    <Button
+                      type="submit"
+                      className="btn-primary"
+                      onClick={handle_transfer}
+                    >
+                      Transfer
+                    </Button>
+                  </Form.Group>
+                </Row>
+              ) : (
+                <></>
+              )}
             </Form>
           </>
         }
